2a25766edae6bad11d35cb43f1b1b1d5d6f2ff97
Arlind-dev <arlind@sulej.ch>
2023-11-24 10:33:06 +0100

    add docker folder

---
+++ docker/prerequisite.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+mkdir data-owncloud data-proxy mysql redis
+
+docker network create --driver bridge --gateway 172.16.0.1 --ip-range 172.16.0.0/24 --subnet 172.16.0.0/16 proxy
+
+cat > ./data-proxy/traefik.yml <<EOF
+api:
+  dashboard: true
+  debug: true
+entryPoints:
+  http:
+    address: ":80"
+    http:
+      redirections:
+        entryPoint:
+          to: https
+          scheme: https
+  https:
+    address: ":443"
+serversTransport:
+  insecureSkipVerify: true
+providers:
+  docker:
+    endpoint: "unix:///var/run/docker.sock"
+    exposedByDefault: false
+  file:
+    filename: /config.yml
+certificatesResolvers:
+  cloudflare:
+    acme:
+      email: yourcloudflare@email.com
+      storage: acme.json
+      dnsChallenge:
+        provider: cloudflare
+        resolvers:
+          - "1.1.1.1:53"
+          - "1.0.0.1:53"
+EOF
+
+touch ./data-proxy/config.yml
+
+sudo chown -R 1000:1000 ./
+
+chmod -R 755 ./
+
+touch ./data-proxy/acme.json
+
+chmod 600 ./data-proxy/acme.json
